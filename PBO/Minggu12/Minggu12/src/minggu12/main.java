9fa070f94caba955b3e4aef6335fb2da3c9f0da9
Dalhats13 <naufal.dalhats.tif20@polban.ac.id>
2021-12-18 15:47:59 +0700

    Push Minggu 11 & 12

---
+++ PBO/Minggu12/Minggu12/src/minggu12/main.java
@@ -0,0 +1,23 @@
+package minggu12;
+
+import java.io.*;
+import java.util.*;
+import com.google.gson.*;
+import com.google.gson.reflect.TypeToken;
+
+public class main {
+	public static void main(String[] args) {
+        Gson gson = new Gson();
+        try {
+            ArrayList<Product> produk = gson.fromJson(
+              new FileReader("D:Kuliah\\Mata Kuliah\\Semester 3\\Praktek\\PBO\\w12\\barang.json"), 
+              new TypeToken<ArrayList<Product>>() {}.getType()
+            );
+            for(int i=0;i<produk.size();i++){
+                System.out.println((i+1) + ". " + produk.get(i).toString());
+            }
+        }catch (JsonIOException | JsonSyntaxException | FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
